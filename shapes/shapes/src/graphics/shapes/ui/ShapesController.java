5870649ba14df4be4d6fc498aa47c876d388b8e3
Getsuren <vernay-jacques@hotmail.fr>
2017-05-20 19:28:01 +0200

    Update ShapesController.java

--- shapes/shapes/src/graphics/shapes/ui/ShapesController.java
+++ shapes/shapes/src/graphics/shapes/ui/ShapesController.java
@@ -1,8 +1,11 @@
 package graphics.shapes.ui;
 
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -26,54 +29,50 @@ public class ShapesController extends Controller {
 				System.out.println(this.selectedShapes.toString());
 			}
 		}
+		/*KeyboardFocusManager kfm = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+		kfm.addKeyEventDispatcher(new KeyEventDispatcher(){
+		      @Override
+		      public boolean dispatchKeyEvent(KeyEvent e) {
+		        System.out.println("Got key event!");
+		        return false;
+		      }}
+		      );*/
 	}
 	@Override
 	public void mouseClicked(MouseEvent e){
 		Shape shape = this.getTarget(e);
-		if (shape != null) {
-			if (this.selectedShapes.isEmpty()){	
-				this.select(shape);
-			}
-			else{
-				if (this.shiftDown()){
-					this.select(shape);
-				}
-				else {
-					this.unselectAll();
-					this.select(shape);
-				}
+		if (this.isShiftDown){
+			if (shape != null) {
+				this.toggleSelection(shape);
 			}
 		}
 		else{
 			this.unselectAll();
+			if (shape!= null){
+				this.toggleSelection(shape);
+			}
 		}
 		getView().updateUI();
 	}
-	/*
 	@Override
 	public void mousePressed(MouseEvent e){
 		Shape shape = this.getTarget(e);
-		if (((SelectionAttributes) shape.getAttributes("Select")).isSelected()) {
+		if (shape != null && this.isSelected(shape)) {
 			
 		}
-	}*/
-	
-	/*
+		if (shape == null || !this.isSelected(shape)){
+			
+		}
+		getView().updateUI();
+	}
 	@Override
 	public void mouseReleased(MouseEvent e){
 		getView().updateUI();
-	}*/
+	}
 	@Override
 	public void mouseDragged(MouseEvent e){
 		this.translateSelected(e.getX(), e.getY());
 		getView().updateUI();
-	}
-	public void translateSelected(int x,int y){
-		Iterator<Shape> iter = this.selectedShapes.iterator();
-		while(iter.hasNext()){
-			Shape shape = iter.next();
-			shape.translate(x - shape.getLoc().x, y - shape.getLoc().y); // soustraction de la position actuelle de la shape pour ne pas partir vers l'infini
-		}
 	}
 	public Shape getTarget(MouseEvent e){
 		SCollection newModel = (SCollection) super.getModel();
@@ -87,16 +86,16 @@ public class ShapesController extends Controller {
 		}
 		return null;
 	}
-	public void unselectAll(){
-		ArrayList<Shape> shapesToBeRemoved = new ArrayList<Shape>();
-		this.selectedShapes.forEach(shapes->{shapesToBeRemoved.add(shapes);}); // on ne peut pas modifier une liste en meme temps qu'on la parcours
-		shapesToBeRemoved.forEach(shapes->{this.unselect(shapes);});
+	public void translate(int x,int y,Shape shape){
+		shape.translate(x - shape.getLoc().x, y - shape.getLoc().y); // soustraction de la position actuelle de la shape pour ne pas partir vers l'infini
 	}
-	/*public void translate(int x,int y){
-		SCollection newModel = (SCollection) super.getModel();
-		Shape shape;
-		Iterator<Shape> iter=newModel.iterator();
-	}*/
+	public void translateSelected(int x,int y){
+		Iterator<Shape> iter = this.selectedShapes.iterator();
+		while(iter.hasNext()){
+			Shape shape = iter.next();
+			this.translate(x, y, shape);
+		}
+	}
 	public void select(Shape shape){
 		this.selectedShapes.add(shape);
 		((SelectionAttributes) shape.getAttributes("Select")).select();
@@ -104,6 +103,11 @@ public class ShapesController extends Controller {
 	public void unselect(Shape shape){
 		this.selectedShapes.remove(shape);
 		((SelectionAttributes) shape.getAttributes("Select")).unselect();
+	}
+	public void unselectAll(){
+		ArrayList<Shape> shapesToBeRemoved = new ArrayList<Shape>();
+		this.selectedShapes.forEach(shapes->{shapesToBeRemoved.add(shapes);}); // on ne peut pas modifier une liste en meme temps qu'on la parcours
+		shapesToBeRemoved.forEach(shapes->{this.unselect(shapes);});
 	}
 	public void toggleSelection(Shape shape){
 		if (this.isSelected(shape)){
@@ -117,19 +121,20 @@ public class ShapesController extends Controller {
 		return ((SelectionAttributes) shape.getAttributes("Select")).isSelected();
 	}
 	@Override
-	public void keypressed(KeyEvent e){
-		System.out.println("merde");
-		 if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-             System.out.println("Right");
-         }
-
-         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-             System.out.println("Left");
-         }
+	public void keyPressed(KeyEvent e){
+		if(e.isShiftDown()){
+			this.isShiftDown = true;
+		}
 	}
 	@Override
-	public void keyreleased(KeyEvent e){
-		this.isShiftDown = e.isShiftDown();
+	public void keyReleased(KeyEvent e){
+		if(!e.isShiftDown()){
+			this.isShiftDown = false;
+		}
+	}
+	@Override
+	public void keyTyped(KeyEvent e){
+		
 	}
 	public boolean shiftDown(){
 		return this.isShiftDown;
