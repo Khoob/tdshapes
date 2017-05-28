09e58b3836d6271756363e9af1bb83395c5118c5
Getsuren <vernay-jacques@hotmail.fr>
2017-05-28 15:41:15 +0200

    Update ShapeDraftman.java

--- shapes/shapes/src/graphics/shapes/ui/ShapeDraftman.java
+++ shapes/shapes/src/graphics/shapes/ui/ShapeDraftman.java
@@ -14,7 +14,9 @@ import graphics.shapes.attributes.SelectionAttributes;
 
 public class ShapeDraftman implements ShapeVisitor {
 	
-	public final ColorAttributes DEFAULTCOLORATTRIBUTES = new ColorAttributes(true, true, Color.BLACK, Color.BLACK);;
+	public final ColorAttributes DEFAULTCOLORATTRIBUTE = new ColorAttributes(true, true, Color.BLACK, Color.BLACK);
+	public final SelectionAttributes DEFAULTSELECTIONATTRIBUTE = new SelectionAttributes();
+	public final FontAttributes DEFAULTFONTATTRIBUTE = new FontAttributes();
 	private Graphics g;
 	
 	public ShapeDraftman(Graphics g){
@@ -25,6 +27,12 @@ public class ShapeDraftman implements ShapeVisitor {
 	public void visitRectangle(SRectangle srect) {
 		SelectionAttributes select = (SelectionAttributes) srect.getAttributes("Select");
 		ColorAttributes color = (ColorAttributes) srect.getAttributes("Color");
+		if (select == null){
+			select = this.DEFAULTSELECTIONATTRIBUTE;
+		}
+		if (color == null){
+			color = this.DEFAULTCOLORATTRIBUTE;
+		}
 		if (color.filled){	
 			g.setColor(color.filledColor);
 			g.fillRect(srect.getLoc().x,srect.getLoc().y, srect.getRect().width, srect.getRect().height);
@@ -44,6 +52,12 @@ public class ShapeDraftman implements ShapeVisitor {
 	public void visitCircle(SCircle scir) {
 		SelectionAttributes select = (SelectionAttributes) scir.getAttributes("Select");
 		ColorAttributes color = (ColorAttributes) scir.getAttributes("Color");
+		if (select == null){
+			select = this.DEFAULTSELECTIONATTRIBUTE;
+		}
+		if (color == null){
+			color = this.DEFAULTCOLORATTRIBUTE;
+		}
 		if (color.filled){
 			g.setColor(color.filledColor);
 			g.fillOval(scir.getLoc().x, scir.getLoc().y, scir.getRadius(), scir.getRadius());
@@ -64,22 +78,32 @@ public class ShapeDraftman implements ShapeVisitor {
 		SelectionAttributes select = (SelectionAttributes) stext.getAttributes("Select");
 		FontAttributes font = (FontAttributes) stext.getAttributes("Font");
 		ColorAttributes color = (ColorAttributes) stext.getAttributes("Color");
+		if (select == null){
+			select = this.DEFAULTSELECTIONATTRIBUTE;
+		}
+		if (font == null){
+			font = this.DEFAULTFONTATTRIBUTE;
+		}
+		if (color == null){
+			color = this.DEFAULTCOLORATTRIBUTE;
+		}
 		if (color.filled){
 			g.setColor(color.filledColor);
-			g.fillRect(stext.getLoc().x, stext.getLoc().y - 10,stext.getBounds().width,stext.getBounds().height);
+			g.fillRect(stext.getBounds().x, stext.getBounds().y, stext.getBounds().width, stext.getBounds().height);
 		}
 		if (color.stroked){
 			g.setColor(color.strokedColor);
-			g.drawRect(stext.getLoc().x, stext.getLoc().y - 10,stext.getBounds().width,stext.getBounds().height);
+			g.drawRect(stext.getBounds().x, stext.getBounds().y, stext.getBounds().width, stext.getBounds().height);
 		}
 		g.setFont(font.font);
 		g.setColor(font.fontColor);
 		g.drawString(stext.getText(), stext.getLoc().x, stext.getLoc().y);
 		if (select.isSelected()){
 			g.setColor(Color.black);
-			g.drawRect(stext.getLoc().x - 2,stext.getLoc().y - 10 - 2, 4, 4);
-			g.drawRect(stext.getLoc().x + stext.getBounds().width - 3,stext.getLoc().y - 10 + stext.getBounds().height - 3, 4, 4);
+			g.drawRect(stext.getBounds().x - 2,stext.getBounds().y - 2, 4, 4);
+			g.drawRect(stext.getBounds().x + stext.getBounds().width - 3,stext.getBounds().y + stext.getBounds().height - 3, 4, 4);
 		}
+		//g.drawRect(stext.getBounds().x, stext.getBounds().y, stext.getBounds().width, stext.getBounds().height);
 	}
 
 	@Override
