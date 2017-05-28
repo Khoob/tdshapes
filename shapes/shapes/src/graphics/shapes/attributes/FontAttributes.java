7aff1ac768a46884f3f393777253decb45ed1bcf
Getsuren <vernay-jacques@hotmail.fr>
2017-05-28 15:42:28 +0200

    Update FontAttributes.java

--- shapes/shapes/src/graphics/shapes/attributes/FontAttributes.java
+++ shapes/shapes/src/graphics/shapes/attributes/FontAttributes.java
@@ -1,18 +1,29 @@
 package graphics.shapes.attributes;
 
+import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.awt.font.LineMetrics;
+
+import graphics.shapes.SCollection;
+import graphics.shapes.ui.ShapesView;
 
 
 
 public class FontAttributes extends Attributes {
-
-	public Font font;
+	
+	public Font font = new Font("Helvetica",Font.PLAIN,12);
 	public Color fontColor = Color.black;
 	
 	public Rectangle getBounds(String text){
-		return new Rectangle();
+		Canvas c = new Canvas();
+		FontMetrics metrics = c.getFontMetrics(font);
+		int hgt = metrics.getHeight();
+		int adv = metrics.stringWidth(text);
+		return new Rectangle(adv +2, hgt +1);
 	}
 	
 	@Override
